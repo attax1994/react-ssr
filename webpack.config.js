51076b9a51a7e52a3f85010495868339ffea7168
attax1994 <lqytyui@qq.com>
2019-02-17 22:22:40 +0800

    initial

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
-const path = require('path');
+var path = require('path')
 
 module.exports = {
   entry: {
     client: './src/client.js',
-    bundle: './src/bundle.js'
+    bundle: './src/bundle.js',
   },
   output: {
-    path: path.resolve(__dirname, 'assets'),
-    filename: "[name].js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
- }
-
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"]
+            },
+          }
+        ],
+      },
+      {
+        test: /\.s?[ca]ss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 }
